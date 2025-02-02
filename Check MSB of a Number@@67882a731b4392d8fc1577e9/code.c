ec016fcef05dd49f91783379207c59e9b2bc3212
samel22 <sameldhingra77@gmail.com>
2025-02-02 15:35:29 +0530

    Solution to Check MSB of a Number

--- Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -2,7 +2,7 @@
 int main() {
     unsigned int a;
     scanf("%u",&a);
-    if(a & (1<<31)){
+    if(a & (1<<31)){  //32-bit integer is represented by 32 binary digits(bits), MSG is leftmost so we used left shift operator
         printf("Set");
     }
     else{
