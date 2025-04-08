a080506bf98d66861d43593a1997fb87108eb40e
Samel Dhingra <sameldhingra77@gmail.com>
2025-04-08 11:36:39 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,29 +1,16 @@
 #include <stdio.h>
 
 int FindPeakElement(int arr[], int N) {
-    // Edge case: only one element
-    if (N == 1) {
-        return arr[0];
-    }
+    if (N == 1) return arr[0];
 
-    // Check first element
-    if (N > 1 && arr[0] >= arr[1]) {
-        return arr[0];
-    }
+    if (N > 1 && arr[0] > arr[1]) return arr[0];
 
-    // Check middle elements
     for (int i = 1; i < N - 1; i++) {
-        if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) {
-            return arr[i];
-        }
+        if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) return arr[i];
     }
 
-    // Check last element
-    if (N > 1 && arr[N - 1] >= arr[N - 2]) {
-        return arr[N - 1];
-    }
-
-    // This should ideally not be reached if the array always has a peak element
+    
+    if (N > 1 && arr[N - 1] > arr[N - 2]) return arr[N - 1];
     return -1;
 }
 
