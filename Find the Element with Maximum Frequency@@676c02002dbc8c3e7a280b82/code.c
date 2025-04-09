4934357fb884367b6714cd9d41d0323b8e239785
Samel Dhingra <sameldhingra77@gmail.com>
2025-04-09 12:10:59 +0530

    Solution to Find the Element with Maximum Frequency

--- Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b82/code.c
+++ Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b82/code.c
@@ -14,11 +14,11 @@ int main(){
                 count++;
             }
         }
-        if(count>N/2) {
+        if(count>=N/2) {
             printf("%d\n",arr[i]);
             return 0;
         }
     }
-    //printf("-1");
+    printf("-1");
 
 }
