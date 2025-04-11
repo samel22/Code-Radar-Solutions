ff0d56def3ca52e2fad925d9c057c620f628e849
Samel Dhingra <sameldhingra77@gmail.com>
2025-04-11 17:28:04 +0530

    Solution to Count the Occurrences of a Character

---
+++ Count the Occurrences of a Character@@676c039d80e1714600f6764b/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int replace(char a, char str[]){
+    int count=0;
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]==a){
+            count++;
+        }
+    }
+    printf("%c",count);
+}
+
+int main(){
+    char str[100], a,b;
+    scanf("%s", str);        // Read string
+    scanf(" %c", &a);        // Read char to replace (space before %c handles newline)     // 
+
+    replace(a,b,str);
+
+}
